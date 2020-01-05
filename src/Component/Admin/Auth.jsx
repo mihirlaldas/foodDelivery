5ae4123e13a51ad11324cec10aefdd3ac0c5a685
mihirlaldas <mihirlaldas@gmail.com>
2020-01-05 19:35:53 +0530

    auth react component added

--- src/Component/Admin/Auth.jsx
+++ src/Component/Admin/Auth.jsx
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
-
-export class Auth extends Component {
-    onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log("Name: " + profile.getName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
-    }
-    render() {
-        return (
-            <div>
-                <div className="g-signin2" data-onsuccess={this.onSignIn}></div>
-            </div>
-        );
-    }
+import React from "react";
+import { GoogleLogin } from "react-google-login";
+const responseGoogle = response => {
+    console.log(response);
+};
+export default function Auth() {
+    return (
+        <div>
+            <GoogleLogin
+                clientId="797271683218-4fm4o6t0bkvj8sudj1kaj4uv4e5npa1i.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={"single_host_origin"}
+            />
+        </div>
+    );
 }
-
-export default Auth;
