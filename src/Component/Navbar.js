0a6c9e60af9cffe451f3f67747c6349bcfbe4ebb
mihirlaldas <mihirlaldas@gmail.com>
2020-03-21 13:22:35 +0530

    hide userlogin

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,58 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
 export default function Navbar() {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link className="navbar-brand" to="/">
-                    <img
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNXwJpWaYqt4WJ267MjlgQvJwNhpYheSgMyehsnEJ_g-x5VmukRw&s"
-                        alt="logo"
-                        width="80px"
-                    />
-                </Link>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNXwJpWaYqt4WJ267MjlgQvJwNhpYheSgMyehsnEJ_g-x5VmukRw&s"
+            alt="logo"
+            width="80px"
+          />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                <div
-                    className="collapse navbar-collapse"
-                    id="navbarSupportedContent"
-                >
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <Link className="nav-link" to="/">
-                                Home <span className="sr-only">(current)</span>
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/orders">
-                                Order History
-                            </Link>
-                        </li>
-                    </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                        <Link to="/auth">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">
+                Home <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/orders">
+                Order History
+              </Link>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            {/* <Link to="/auth">
                             <button className="btn btn-outline-primary my-2 my-sm-0">
                                 User Login
                             </button>
-                        </Link>
-                        <Link to="/dashboard">
-                            <button className="btn btn-outline-success my-2 my-sm-0">
-                                Dashboard
-                            </button>
-                        </Link>
-                    </form>
-                </div>
-            </nav>
+                        </Link> */}
+            <Link to="/dashboard">
+              <button className="btn btn-outline-success my-2 my-sm-0">
+                Dashboard
+              </button>
+            </Link>
+          </form>
         </div>
-    );
+      </nav>
+    </div>
+  );
 }
